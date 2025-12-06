d0ad65ea6af81e5b0a9f86ef9a98684fd4ac033a
tianriw <nodevoid233@gmail.com>
2025-12-06 18:00:28 +0800

    发布

--- main.py
+++ main.py
@@ -29,7 +29,6 @@ class RandE621(Star):
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
     @filter.command("rand621")
     async def rand621(self, event: AstrMessageEvent):
         """发送随机 E621 图片（标记为 S 或 Q）""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
